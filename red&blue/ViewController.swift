ce7d08e8edef086c081d211b2e601685f8833675
Evan Sugita <evanmayo@evans-macbook-pro.local>
2015-09-22 12:48:19 -0700

    Initial Commit

---
+++ red&blue/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  red&blue
+//
+//  Created by Evan Leong on 9/22/15.
+//  Copyright © 2015 Evan Leong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
